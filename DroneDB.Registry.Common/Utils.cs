ed7afa9b7ec79e2d7c874cdfa5b0e03eb27f1fe7
Luca Di Leo <ldileo@digipa.it>
2020-07-10 17:02:39 +0200

    Check and fix config if missing or empty secret

---
+++ DroneDB.Registry.Common/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DroneDB.Registry.Common
+{
+    public static class Utils
+    {
+        public static string RandomString(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] uintBuffer = new byte[sizeof(uint)];
+
+                while (length-- > 0)
+                {
+                    rng.GetBytes(uintBuffer);
+                    uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                    res.Append(valid[(int)(num % (uint)valid.Length)]);
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+}
