4116d24ed59f492bc566c3ec8f4b1147939ae88a
Piero Toffanin <pt@masseranolabs.com>
2020-07-13 13:44:09 -0400

    Use FromForm for API params in POST requests

--- Registry.Web/Controllers/UsersController.cs
+++ Registry.Web/Controllers/UsersController.cs
@@ -24,7 +24,7 @@ namespace Registry.Web.Controllers
 
         [AllowAnonymous]
         [HttpPost("authenticate")]
-        public IActionResult Authenticate([FromBody]AuthenticateRequest model)
+        public IActionResult Authenticate([FromForm]AuthenticateRequest model)
         {
             var response = _userService.Authenticate(model);
 
