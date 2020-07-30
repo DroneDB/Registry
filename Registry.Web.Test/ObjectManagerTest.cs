cf71d053f2587fca7c6696f2a981452fb7b48b89
Luca Di Leo <ldileo@digipa.it>
2020-07-30 18:00:12 +0200

    Added test stub

---
+++ Registry.Web.Test/ObjectManagerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Registry.Web.Services.Adapters;
+using Registry.Web.Services.Ports;
+
+namespace Registry.Web.Test
+{
+    public class ObjectManagerTest
+    {
+        private Mock<ILogger<IObjectsManager>> _loggerMock = new Mock<ILogger<IObjectsManager>>();
+        private Mock<IObjectsManager> _objectsManagerMock = new Mock<IObjectsManager>();
+        private IUtils _utils = new WebUtils();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetAllTest()
+        {
+
+
+            Assert.Pass();
+        }
+    }
+}
