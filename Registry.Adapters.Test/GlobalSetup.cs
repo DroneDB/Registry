151685c472066ecca86d8b2493917d15f3359c26
Luca Di Leo <admin@lucadileo.it>
2020-08-14 09:41:48 +0200

    Fixed NUnit 3 wrong test folder

---
+++ Registry.Adapters.Test/GlobalSetup.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace Registry.Adapters.Test
+{
+    [SetUpFixture]
+    public class GlobalSetup
+    {
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
+        }
+    }
+}
