7f411570fc096ef099f5a106985a25fa68b6132b
Luca Di Leo <admin@lucadileo.it>
2020-09-10 18:34:54 +0200

    Added Id field to registry Entry

--- Registry.Web.Data/Models/Batch.cs
+++ Registry.Web.Data/Models/Batch.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Text;
-using Registry.Common;
 
 namespace Registry.Web.Data.Models
 {
@@ -32,24 +31,4 @@ namespace Registry.Web.Data.Models
     {
         Running, Committed, Rolledback
     }
-
-
-    public class Entry
-    {
-        [Key]
-        public string Path { get; set; }
-        
-        [Required]
-        public string Hash { get; set; }
-        [Required]
-        public EntryType Type { get; set; }
-        [Required]
-        public int Size { get; set; }
-        [Required]
-        public DateTime AddedOn { get; set; }
-
-        [Required]
-        public Batch Batch { get; set; }
-    }
-
 }
---
+++ Registry.Web.Data/Models/Entry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Registry.Common;
+
+namespace Registry.Web.Data.Models
+{
+    public class Entry
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Path { get; set; }
+        
+        [Required]
+        public string Hash { get; set; }
+        [Required]
+        public EntryType Type { get; set; }
+        [Required]
+        public int Size { get; set; }
+        [Required]
+        public DateTime AddedOn { get; set; }
+
+        [Required]
+        public Batch Batch { get; set; }
+    }
+}
