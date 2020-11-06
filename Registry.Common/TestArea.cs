2843bb51b1c82fec3bbffc2269b0472442bf6958
Luca Di Leo <ldileo@digipa.it>
2020-11-06 20:14:09 +0100

    Added TestArea util class

---
+++ Registry.Common/TestArea.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Registry.Common
+{
+    public class TestArea : IDisposable
+    {
+        public string Name { get; }
+
+        public string TestFolder { get; }
+
+        public TestArea(string name)
+        {
+            Name = name;
+            TestFolder = Path.Combine(Path.GetTempPath(), nameof(TestArea), $"{name}-{CommonUtils.RandomString(8)}");
+
+            Directory.CreateDirectory(TestFolder);
+
+            Debug.WriteLine($"Created test area '{name}' in folder '{TestFolder}'");
+
+        }
+
+        public TestArea() : this(new StackFrame(1).GetMethod().Name)
+        {
+            //
+        }
+
+        public void Dispose()
+        {
+            if (Directory.Exists(TestFolder))
+                Directory.Delete(TestFolder, true);
+
+            Debug.WriteLine($"Disposed test area '{Name}' in folder '{TestFolder}'");
+
+        }
+    }
+}
