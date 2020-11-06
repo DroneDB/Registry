9dfcbf1f7b83687014b0d83917583667be67c6fa
Luca Di Leo <ldileo@digipa.it>
2020-11-06 20:14:37 +0100

    Fixes to ChunkedUploadManager to address concurrency issues

--- Registry.Web/Services/Adapters/ChunkedUploadManager.cs
+++ Registry.Web/Services/Adapters/ChunkedUploadManager.cs
@@ -40,7 +40,7 @@ namespace Registry.Web.Services.Adapters
             if (chunks < 1)
                 throw new ArgumentException($"Chunks count cannot be lower than 1 ({chunks})");
 
-            if (size < 0)
+            if (size <= 0)
                 throw new ArgumentException($"Size cannot be lower than 0 ({size})");
 
             _logger.LogInformation($"Starting upload session of '{fileName}' with size {size} in {chunks} chunks");
@@ -73,111 +73,134 @@ namespace Registry.Web.Services.Adapters
                 throw new ArgumentException("Cannot read from stream");
 
             // Safety net
-            using var mutex = new Mutex(true, $"ChunkedUploadSession-Upload-{sessionId}");
+            using var mutex = new Mutex(false, $"ChunkedUploadSession-Upload-{sessionId}");
+
             if (!mutex.WaitOne(TimeSpan.FromMinutes(1)))
                 throw new InvalidOperationException($"Multiple call overlap of Upload with id {sessionId} on index {index}");
-
-            var session = _context.UploadSessions.FirstOrDefault(item => item.Id == sessionId);
-
-            if (session == null)
-                throw new ArgumentException($"Cannot find upload session {sessionId}");
-
-            if (session.EndedOn != null)
-                throw new ArgumentException("Cannot upload to a closed session");
-
-            if (index >= session.ChunksCount)
-                throw new ArgumentException($"Invalid chunk index {index} out of {session.ChunksCount}");
-
-            _logger.LogInformation($"In session {session.Id} uploading chunk {index} out of {session.ChunksCount}");
-
-            var tempFileName = string.Format(TempFileNameFormat, sessionId, session.FileName, index);
-            var tempFilePath = Path.Combine(_settings.UploadPath, tempFileName);
-            _logger.LogInformation($"Temp file '{tempFilePath}' in '{Path.GetFullPath(tempFilePath)}'");
-
-            _context.Entry(session).Collection(item => item.Chunks).Load();
-            var fileChunk = session.Chunks.FirstOrDefault(item => item.Index == index);
-
-            if (fileChunk != null)
+            try
             {
-                _logger.LogInformation($"Chunk {index} already existing, replacing it");
-                fileChunk.Date = DateTime.Now;
-                fileChunk.Size = chunkStream.Length;
-            }
-            else
-            {
-                _logger.LogInformation($"Chunk {index} does not exist, creating it");
-                fileChunk = new FileChunk
+
+                var session = _context.UploadSessions.FirstOrDefault(item => item.Id == sessionId);
+
+                if (session == null)
+                    throw new ArgumentException($"Cannot find upload session {sessionId}");
+
+                if (session.EndedOn != null)
+                    throw new ArgumentException("Cannot upload to a closed session");
+
+                if (index >= session.ChunksCount)
+                    throw new ArgumentException($"Invalid chunk index {index}, range [0, {session.ChunksCount - 1}]");
+
+                _logger.LogInformation($"In session {session.Id} uploading chunk {index} out of {session.ChunksCount}");
+
+                var tempFileName = string.Format(TempFileNameFormat, sessionId, session.FileName, index);
+                var tempFilePath = Path.Combine(_settings.UploadPath, tempFileName);
+                _logger.LogInformation($"Temp file '{tempFilePath}' in '{Path.GetFullPath(tempFilePath)}'");
+
+                _context.Entry(session).Collection(item => item.Chunks).Load();
+                var fileChunk = session.Chunks.FirstOrDefault(item => item.Index == index);
+
+                if (fileChunk != null)
                 {
-                    Date = DateTime.Now,
-                    Index = index,
-                    Session = session,
-                    Size = chunkStream.Length
-                };
-                _context.FileChunks.Add(fileChunk);
-            }
-            _context.SaveChanges();
+                    _logger.LogInformation($"Chunk {index} already existing, replacing it");
+                    fileChunk.Date = DateTime.Now;
+                    fileChunk.Size = chunkStream.Length;
+                }
+                else
+                {
+                    _logger.LogInformation($"Chunk {index} does not exist, creating it");
+                    fileChunk = new FileChunk
+                    {
+                        Date = DateTime.Now,
+                        Index = index,
+                        Session = session,
+                        Size = chunkStream.Length
+                    };
+                    _context.FileChunks.Add(fileChunk);
+                }
+                _context.SaveChanges();
 
-            if (File.Exists(tempFilePath))
+                if (File.Exists(tempFilePath))
+                {
+                    _logger.LogInformation("Temp file exists, removing it");
+                    File.Delete(tempFilePath);
+                }
+                else
+                    _logger.LogInformation("Temp file does not exist");
+
+                // Write temp file
+                using var tmpFile = File.OpenWrite(tempFilePath);
+                chunkStream.CopyTo(tmpFile);
+            }
+            finally
             {
-                _logger.LogInformation("Temp file exists, removing it");
-                File.Delete(tempFilePath);
+                mutex.ReleaseMutex();
             }
-            else
-                _logger.LogInformation("Temp file does not exist");
-
-            // Write temp file
-            using var tmpFile = File.OpenWrite(tempFilePath);
-            chunkStream.CopyTo(tmpFile);
         }
 
         public string CloseSession(int sessionId)
         {
             // Safety net
-            using var mutex = new Mutex(true, $"ChunkedUploadSession-Close-{sessionId}");
+            using var mutex = new Mutex(false, $"ChunkedUploadSession-Close-{sessionId}");
+
             if (!mutex.WaitOne(TimeSpan.FromMinutes(1)))
                 throw new InvalidOperationException($"Multiple call overlap of CloseSession with id {sessionId}");
 
-            var session = _context.UploadSessions.FirstOrDefault(item => item.Id == sessionId);
-
-            if (session == null)
-                throw new ArgumentException($"Cannot find upload session {sessionId}");
-
-            if (session.EndedOn != null)
-                throw new ArgumentException("Session already closed");
-
-            _context.Entry(session).Collection(item => item.Chunks).Load();
-
-            var chunks = session.Chunks.OrderBy(chunk => chunk.Index).ToArray();
-
-            if (chunks.Length != session.ChunksCount)
-                throw new InvalidOperationException($"Expected {session.ChunksCount} chunks but got only {chunks}");
-
-            _logger.LogInformation($"Closing session {sessionId} of file {session.FileName}");
-
-            // All this only to check if the indexes are a contiguous non-repetitive sequence starting from 0
-            if (Enumerable.Range(0, session.ChunksCount).Any(index => chunks.All(chunk => chunk.Index != index)))
-                throw new InvalidOperationException(
-                    $"Expected chunks from 0 to {session.ChunksCount - 1} but got {string.Join(" ", chunks.Select(chunk => chunk.Index.ToString()))}");
-
-            var targetFilePath = Path.Combine(_settings.UploadPath, session.FileName);
-
-            _logger.LogInformation($"Destination file path '{targetFilePath}'");
-
-            using var dest = File.OpenWrite(targetFilePath);
-            foreach (var chunk in chunks)
+            try
             {
-                var tempFileName = string.Format(TempFileNameFormat, sessionId, session.FileName, chunk.Index);
-                var tempFilePath = Path.Combine(_settings.UploadPath, tempFileName);
+                var session = _context.UploadSessions.FirstOrDefault(item => item.Id == sessionId);
 
-                // Copy content of temp file to dest file
-                using var tmp = File.OpenRead(tempFilePath);
-                tmp.CopyTo(dest);
+                if (session == null)
+                    throw new ArgumentException($"Cannot find upload session {sessionId}");
+
+                if (session.EndedOn != null)
+                    throw new ArgumentException("Session already closed");
+
+                _context.Entry(session).Collection(item => item.Chunks).Load();
+
+                var chunks = session.Chunks.OrderBy(chunk => chunk.Index).ToArray();
+
+                if (chunks.Length != session.ChunksCount)
+                    throw new InvalidOperationException($"Expected {session.ChunksCount} chunks but got only {chunks}");
+
+                _logger.LogInformation($"Closing session {sessionId} of file {session.FileName}");
+
+                // All this only to check if the indexes are a contiguous non-repetitive sequence starting from 0
+                if (Enumerable.Range(0, session.ChunksCount).Any(index => chunks.All(chunk => chunk.Index != index)))
+                    throw new InvalidOperationException(
+                        $"Expected chunks from 0 to {session.ChunksCount - 1} but got {string.Join(" ", chunks.Select(chunk => chunk.Index.ToString()))}");
+
+                var targetFilePath = Path.Combine(_settings.UploadPath, session.FileName);
+
+                _logger.LogInformation($"Destination file path '{targetFilePath}'");
+
+                using (var dest = File.OpenWrite(targetFilePath))
+                {
+                    foreach (var chunk in chunks)
+                    {
+                        var tempFileName = string.Format(TempFileNameFormat, sessionId, session.FileName, chunk.Index);
+                        var tempFilePath = Path.Combine(_settings.UploadPath, tempFileName);
+
+                        // Copy content of temp file to dest file
+                        using var tmp = File.OpenRead(tempFilePath);
+                        tmp.CopyTo(dest);
+                    }
+                }
+
+                var info = new FileInfo(targetFilePath);
+                if (info.Length != session.TotalSize)
+                    throw new InvalidOperationException(
+                        $"Chunks merge failed: expected file size {session.TotalSize} but got {info.Length}");
+
+                session.EndedOn = DateTime.Now;
+                _context.SaveChanges();
+
+                return targetFilePath;
             }
-
-            session.EndedOn = DateTime.Now;
-            _context.SaveChanges();
-
-            return targetFilePath;
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
 
         }
 
