a0e5f5ef88479ca03da2ac6ed4c559ba97d2ff62
Luca Di Leo <ldileo@digipa.it>
2020-11-19 19:38:56 +0100

    Added entity to store temporary download packages

---
+++ Registry.Web.Data/Models/DownloadPackage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Registry.Web.Data.Models
+{
+    public class DownloadPackage
+    {
+        [Required]
+        public DateTime CreationDate { get; set; }
+        
+        /// <summary>
+        /// Link expiration date, if null the package allows only one download
+        /// </summary>
+        public DateTime? ExpirationDate { get; set; }
+
+        [Required]
+        public Dataset Dataset { get; set; }
+
+        [Required]
+        public string[] Queries { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+    }
+}
