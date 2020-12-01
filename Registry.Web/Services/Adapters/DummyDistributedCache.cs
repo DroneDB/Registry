274ae5d40ec3ee58e176310da28984b7b696e864
Luca Di Leo <ldileo@digipa.it>
2020-12-01 19:09:09 +0100

    Added caching support classes

---
+++ Registry.Web/Services/Adapters/DummyDistributedCache.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Registry.Web.Services.Adapters
+{
+    public class DummyDistributedCache :IDistributedCache
+    {
+        public byte[] Get(string key)
+        {
+            return null;
+        }
+
+        public async Task<byte[]> GetAsync(string key, CancellationToken token = new CancellationToken())
+        {
+            return null;
+        }
+
+        public void Set(string key, byte[] value, DistributedCacheEntryOptions options)
+        {
+            
+        }
+
+        public async Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options,
+            CancellationToken token = new CancellationToken())
+        {
+            
+        }
+
+        public void Refresh(string key)
+        {
+            
+        }
+
+        public async Task RefreshAsync(string key, CancellationToken token = new CancellationToken())
+        {
+            
+        }
+
+        public void Remove(string key)
+        {
+            
+        }
+
+        public async Task RemoveAsync(string key, CancellationToken token = new CancellationToken())
+        {
+            
+        }
+    }
+}
---
+++ Registry.Web/Services/DistributedCacheHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Registry.Ports.DroneDB;
+using Registry.Web.Services.Adapters;
+
+namespace Registry.Web.Services
+{
+    public static class DistributedCacheHelpers
+    {
+        public static IDdb UseCache(this IDdb ddb, IDistributedCache distributedCache)
+        {
+            return new CachedDdb(ddb, distributedCache);
+        }
+    }
+}
