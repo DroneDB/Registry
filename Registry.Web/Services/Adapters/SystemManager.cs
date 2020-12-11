a0a0cfbed1ec6908a2b3353ebfc45224e942e750
Luca Di Leo <ldileo@digipa.it>
2020-12-11 17:16:01 +0100

    Cleanup sessions endpoint is now restricted to admins

--- Registry.Web/Services/Adapters/SystemManager.cs
+++ Registry.Web/Services/Adapters/SystemManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Registry.Web.Exceptions;
 using Registry.Web.Models.DTO;
 using Registry.Web.Services.Ports;
 
@@ -10,14 +11,19 @@ namespace Registry.Web.Services.Adapters
     public class SystemManager : ISystemManager
     {
         private readonly IChunkedUploadManager _chunkedUploadManager;
+        private readonly IAuthManager _authManager;
 
-        public SystemManager(IChunkedUploadManager chunkedUploadManager)
+        public SystemManager(IChunkedUploadManager chunkedUploadManager, IAuthManager authManager)
         {
             _chunkedUploadManager = chunkedUploadManager;
+            _authManager = authManager;
         }
 
         public async Task<CleanupResult> CleanupSessions()
         {
+
+            if (!await _authManager.IsUserAdmin())
+                throw new UnauthorizedException("Only admins can perform system related tasks");
             
             var removedSessions = new List<int>();
             removedSessions.AddRange(await _chunkedUploadManager.RemoveTimedoutSessions());
