5b26c3b7c23104e677dc29b813217febf29dd021
Luca Di Leo <ldileo@digipa.it>
2021-03-16 17:27:53 +0100

    Added push controller endpoints

---
+++ Registry.Web/Controllers/PushController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Registry.Web.Models;
+using Registry.Web.Services.Ports;
+using Registry.Web.Utilities;
+
+namespace Registry.Web.Controllers
+{
+    [ApiController]
+    [Route(RoutesHelper.OrganizationsRadix + "/" +
+           RoutesHelper.OrganizationSlug + "/" +
+           RoutesHelper.DatasetRadix + "/" +
+           RoutesHelper.DatasetSlug + "/" +
+           RoutesHelper.PushRadix)]
+    public class PushController : ControllerBaseEx
+    {
+        private readonly IPushManager _pushManager;
+        private readonly ILogger<PushController> _logger;
+
+        public PushController(IPushManager pushManager, ILogger<PushController> logger)
+        {
+            _pushManager = pushManager;
+            _logger = logger;
+        }
+
+        [HttpPost("init")]
+        public async Task<IActionResult> Init([FromRoute] string orgSlug, [FromRoute] string dsSlug, IFormFile file)
+        {
+            try
+            {
+                _logger.LogDebug($"Push controller Init('{orgSlug}', '{dsSlug}', '{file?.FileName}')");
+
+                if (file == null)
+                    throw new ArgumentException("No file uploaded");
+
+                await using var stream = file.OpenReadStream();
+                
+                var res = await _pushManager.Init(orgSlug, dsSlug, stream);
+
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Exception in Push controller Init('{orgSlug}', '{dsSlug}', '{file?.FileName}')");
+
+                return ExceptionResult(ex);
+            }
+
+
+        }
+
+        [HttpPost("obj")]
+        public async Task<IActionResult> Upload([FromRoute] string orgSlug, [FromRoute] string dsSlug, IFormFile file)
+        {
+            try
+            {
+                _logger.LogDebug($"Push controller Upload('{orgSlug}', '{dsSlug}', '{file?.FileName}')");
+
+                if (file == null)
+                    throw new ArgumentException("No file uploaded");
+
+                await using var stream = file.OpenReadStream();
+                await _pushManager.Upload(orgSlug, dsSlug, stream);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Exception in Push controller Upload('{orgSlug}', '{dsSlug}', '{file?.FileName}')");
+
+                return ExceptionResult(ex);
+            }
+        }
+
+        [HttpPost("commit")]
+        public async Task<IActionResult> Commit([FromRoute] string orgSlug, [FromRoute] string dsSlug)
+        {
+            try
+            {
+                _logger.LogDebug($"Push controller Commit('{orgSlug}', '{dsSlug}')");
+
+                var res = await _pushManager.Commit(orgSlug, dsSlug);
+
+                return Ok(res);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Exception in Push controller Commit('{orgSlug}', '{dsSlug}')");
+
+                return ExceptionResult(ex);
+            }
+        }
+
+
+    }
+}
