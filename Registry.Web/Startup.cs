1ad6873db2861bcf1b604ae856103020694c80b1
Luca Di Leo <ldileo@digipa.it>
2021-05-06 19:21:14 +0200

    Removed session functions

--- Registry.Web/Startup.cs
+++ Registry.Web/Startup.cs
@@ -212,7 +212,6 @@ namespace Registry.Web
             services.AddScoped<IUtils, WebUtils>();
             services.AddScoped<IAuthManager, AuthManager>();
 
-            services.AddScoped<IChunkedUploadManager, ChunkedUploadManager>();
             services.AddScoped<IUsersManager, UsersManager>();
             services.AddScoped<IOrganizationsManager, OrganizationsManager>();
             services.AddScoped<IDatasetsManager, DatasetsManager>();
