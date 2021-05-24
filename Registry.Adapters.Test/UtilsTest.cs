80cb5679239c4d95cd6ce8b7588f18c40866fe93
Luca Di Leo <ldileo@digipa.it>
2021-05-24 18:16:54 +0200

    Added explicit random test

--- Registry.Adapters.Test/UtilsTest.cs
+++ Registry.Adapters.Test/UtilsTest.cs
@@ -15,14 +15,14 @@ namespace Registry.Adapters.Test
     public class UtilsTest
     {
 
-        private static readonly string TestString =  new string(Enumerable.Range(0, 1024 * 1024 * 5 + 731).Select(item => 'A').ToArray());
+        private static readonly string TestString = new string(Enumerable.Range(0, 1024 * 1024 * 5 + 731).Select(item => 'A').ToArray());
 
         [Test]
         public void CalculateMultipartEtag_ShortFile_Ok()
         {
 
             //const string expectedEtag1 = "e6065c4aa2ab1603008fc18410f579d4e6065c4aa2ab1603008fc18410f579d4e6065c4aa2ab1603008fc18410f579d4e6065c4aa2ab1603008fc18410f579d4e6065c4aa2ab1603008fc18410f579d4-5";
-            
+
             using var testArea = new TestArea(nameof(CalculateMultipartEtag_ShortFile_Ok));
 
             var testFile = Path.Combine(testArea.TestFolder, "temp.txt");
@@ -48,6 +48,18 @@ namespace Registry.Adapters.Test
             cc.GetConditions().Should().BeEquivalentTo(newcc.GetConditions());
         }
 
+        [Test]
+        [Explicit]
+        public void RandomString_Ok()
+        {
+            var lst = new List<string>();
+            for (var n = 0; n < 10000; n++)
+            {
+                lst.Add(CommonUtils.RandomString(16));
+            }
+
+            lst.Count.Should().Be(lst.Distinct().Count());
+        }
 
     }
 }
