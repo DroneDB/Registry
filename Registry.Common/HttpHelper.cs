d1c93a1664494a49a62147ab38c9b8c7169230f3
Luca Di Leo <ldileo@digipa.it>
2022-01-27 08:28:01 -0800

    Fix CI

--- Registry.Common/HttpHelper.cs
+++ Registry.Common/HttpHelper.cs
@@ -9,15 +9,10 @@ public static class HttpHelper
 {
     private static readonly HttpClient _httpClient = new HttpClient();
 
-    public static async Task DownloadFileAsync(string uri
-        , string outputPath)
+    public static async Task DownloadFileAsync(string uri, string outputPath)
     {
         if (!Uri.TryCreate(uri, UriKind.Absolute, out _))
             throw new InvalidOperationException("URI is invalid.");
-
-        if (!File.Exists(outputPath))
-            throw new FileNotFoundException("File not found."
-                , nameof(outputPath));
 
         var fileBytes = await _httpClient.GetByteArrayAsync(uri);
         await File.WriteAllBytesAsync(outputPath, fileBytes);
