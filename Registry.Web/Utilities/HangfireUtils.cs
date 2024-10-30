b93a9f5b6f1932b4df8f9f09ae095c08cd14c987
Luca Di Leo <admin@lucadileo.it>
2024-10-30 15:59:23 +0100

    Reduced expiration of dummy job to 5 minutes

--- Registry.Web/Utilities/HangfireUtils.cs
+++ Registry.Web/Utilities/HangfireUtils.cs
@@ -112,7 +112,7 @@ public static class HangfireUtils
     }
 
     [AutomaticRetry(Attempts = 1, OnAttemptsExceeded = AttemptsExceededAction.Fail)]
-    [JobExpiration(ExpirationTimeoutInMinutes = 15)]
+    [JobExpiration(ExpirationTimeoutInMinutes = 5)]
     public static void DummyJob(PerformContext context)
     {
         using var connection = JobStorage.Current.GetConnection();
