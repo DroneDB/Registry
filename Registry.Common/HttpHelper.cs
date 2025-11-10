50b6051f9934127c4a6366514b8e971edcf3eef5
HeDo <ldileo@digipa.it>
2025-11-10 18:04:06 +0100

    Improved download utils

--- Registry.Common/HttpHelper.cs
+++ Registry.Common/HttpHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Registry.Common;
@@ -9,12 +11,35 @@ public static class HttpHelper
 {
     private static readonly HttpClient _httpClient = new HttpClient();
 
-    public static async Task DownloadFileAsync(string uri, string outputPath)
+    public static Task DownloadFileAsync(string uri, string outputPath)
+    {
+        return DownloadFileAsync(uri, outputPath, null);
+    }
+
+    public static async Task DownloadFileAsync(string uri, string outputPath, Dictionary<string, string> headers)
     {
         if (!Uri.TryCreate(uri, UriKind.Absolute, out _))
             throw new InvalidOperationException("URI is invalid.");
 
-        var fileBytes = await _httpClient.GetByteArrayAsync(uri);
-        await File.WriteAllBytesAsync(outputPath, fileBytes);
+        using var request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+        // Add custom headers if provided
+        if (headers != null)
+        {
+            foreach (var header in headers)
+            {
+                request.Headers.TryAddWithoutValidation(header.Key, header.Value);
+            }
+        }
+
+        // Use streaming to avoid loading entire file in memory
+        using var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+        response.EnsureSuccessStatusCode();
+
+        await using var sourceStream = await response.Content.ReadAsStreamAsync();
+        await using var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write, FileShare.None,
+            bufferSize: 8192, useAsync: true);
+
+        await sourceStream.CopyToAsync(fileStream);
     }
 }
