88176040a1ebfb7ad6899a3ae7ea69fc3d386c8e
HeDo <ldileo@digipa.it>
2025-11-28 12:13:55 +0100

    Refactor

--- Registry.Web/Exceptions/QuotaExceededException.cs
+++ Registry.Web/Exceptions/QuotaExceededException.cs
@@ -4,11 +4,8 @@ using Registry.Common;
 
 namespace Registry.Web.Exceptions;
 
-[Serializable]
 internal class QuotaExceededException : InvalidOperationException
 {
-    private long userStorage;
-    private long? maxStorage;
 
     public QuotaExceededException()
     {
@@ -21,15 +18,10 @@ internal class QuotaExceededException : InvalidOperationException
     public QuotaExceededException(long userStorage, long? maxStorage) : base(
         $"Storage quota exceeded: {CommonUtils.GetBytesReadable(userStorage)} out of {(maxStorage == null ? "UNLIMITED" : CommonUtils.GetBytesReadable(maxStorage.Value))}")
     {
-        this.userStorage = userStorage;
-        this.maxStorage = maxStorage;
     }
 
     public QuotaExceededException(string message, Exception innerException) : base(message, innerException)
     {
     }
 
-    protected QuotaExceededException(SerializationInfo info, StreamingContext context) : base(info, context)
-    {
-    }
 }
