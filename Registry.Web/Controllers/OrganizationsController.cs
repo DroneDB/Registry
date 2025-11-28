26326012da6fbcfea3de8830f61e94846dd0d569
HeDo <ldileo@digipa.it>
2025-11-28 09:44:33 +0100

    Improved decorations in OrganizationsController

--- Registry.Web/Controllers/OrganizationsController.cs
+++ Registry.Web/Controllers/OrganizationsController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,8 +20,12 @@ using Registry.Web.Utilities;
 
 namespace Registry.Web.Controllers;
 
+/// <summary>
+/// Controller for managing organizations.
+/// </summary>
 [ApiController]
 [Route(RoutesHelper.OrganizationsRadix)]
+[Produces("application/json")]
 public class OrganizationsController : ControllerBaseEx
 {
     private readonly IOrganizationsManager _organizationsManager;
@@ -31,8 +37,14 @@ public class OrganizationsController : ControllerBaseEx
         _logger = logger;
     }
 
-    [HttpGet(Name = nameof(OrganizationsController) + "." + nameof(GetAll))]
-    [ProducesResponseType(typeof(IEnumerable<OrganizationDto>), 200)]
+    /// <summary>
+    /// Gets all organizations.
+    /// </summary>
+    /// <returns>A list of organizations.</returns>
+    [HttpGet(Name = nameof(GetAll))]
+    [ProducesResponseType(typeof(IEnumerable<OrganizationDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
     public async Task<IActionResult> GetAll()
     {
         try
@@ -49,9 +61,17 @@ public class OrganizationsController : ControllerBaseEx
         }
     }
 
-    [HttpGet(RoutesHelper.OrganizationSlug, Name = nameof(OrganizationsController) + "." + nameof(Get))]
-    [ProducesResponseType(typeof(OrganizationDto), 200)]
-    public async Task<IActionResult> Get(string orgSlug)
+    /// <summary>
+    /// Gets a specific organization by slug.
+    /// </summary>
+    /// <param name="orgSlug">The organization slug.</param>
+    /// <returns>The organization information.</returns>
+    [HttpGet(RoutesHelper.OrganizationSlug, Name = nameof(Get))]
+    [ProducesResponseType(typeof(OrganizationDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Get([FromRoute, Required] string orgSlug)
     {
         try
         {
@@ -68,52 +88,79 @@ public class OrganizationsController : ControllerBaseEx
 
     }
 
-    [HttpPost]
-    [ProducesResponseType(typeof(OrganizationDto), 201)]
-    public async Task<IActionResult> Post([FromForm] OrganizationDto organization)
+    /// <summary>
+    /// Creates a new organization.
+    /// </summary>
+    /// <param name="organization">The organization data.</param>
+    /// <returns>The newly created organization.</returns>
+    [HttpPost(Name = nameof(Create))]
+    [ProducesResponseType(typeof(OrganizationDto), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> Create([FromForm, Required] OrganizationDto organization)
     {
 
         try
         {
-            _logger.LogDebug("Organizations controller Post('{organization?.Slug}')", organization?.Slug);
+            _logger.LogDebug("Organizations controller Create('{organization?.Slug}')", organization?.Slug);
 
             var newOrg = await _organizationsManager.AddNew(organization);
-            return CreatedAtRoute(nameof(OrganizationsController) + "." + nameof(Get), new { orgSlug = newOrg.Slug },
+            return CreatedAtRoute(nameof(Get), new { orgSlug = newOrg.Slug },
                 newOrg);
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Exception in Organizations controller Post('{organization?.Slug}')", organization?.Slug);
+            _logger.LogError(ex, "Exception in Organizations controller Create('{organization?.Slug}')", organization?.Slug);
 
             return ExceptionResult(ex);
         }
 
     }
 
-    [HttpPut(RoutesHelper.OrganizationSlug)]
-    [ProducesResponseType(204)]
-    public async Task<IActionResult> Put(string orgSlug, [FromForm] OrganizationDto organization)
+    /// <summary>
+    /// Updates an existing organization.
+    /// </summary>
+    /// <param name="orgSlug">The organization slug.</param>
+    /// <param name="organization">The organization update data.</param>
+    /// <returns>No content on success.</returns>
+    [HttpPut(RoutesHelper.OrganizationSlug, Name = nameof(Update))]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update(
+        [FromRoute, Required] string orgSlug,
+        [FromForm, Required] OrganizationDto organization)
     {
 
         try
         {
-            _logger.LogDebug("Organizations controller Put('{OrgSlug}', {organization?.Slug}')",orgSlug, organization?.Slug);
+            _logger.LogDebug("Organizations controller Update('{OrgSlug}', {organization?.Slug}')",orgSlug, organization?.Slug);
 
             await _organizationsManager.Edit(orgSlug, organization);
             return NoContent();
         }
         catch (Exception ex)
         {
-            _logger.LogError(ex, "Exception in Organizations controller Put('{OrgSlug}', {organization?.Slug}')", orgSlug, organization?.Slug);
+            _logger.LogError(ex, "Exception in Organizations controller Update('{OrgSlug}', {organization?.Slug}')", orgSlug, organization?.Slug);
 
             return ExceptionResult(ex);
         }
 
     }
 
-    [HttpDelete(RoutesHelper.OrganizationSlug)]
-    [ProducesResponseType(204)]
-    public async Task<IActionResult> Delete(string orgSlug)
+    /// <summary>
+    /// Deletes an organization.
+    /// </summary>
+    /// <param name="orgSlug">The organization slug.</param>
+    /// <returns>No content on success.</returns>
+    [HttpDelete(RoutesHelper.OrganizationSlug, Name = nameof(Delete))]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete([FromRoute, Required] string orgSlug)
     {
 
         try
